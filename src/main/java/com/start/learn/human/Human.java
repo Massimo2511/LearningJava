c33d813ac0505a00a8b79ee22ef606b41b6458c2
m.panichek <maksym.panicheck@skelia.com>
2022-07-09 21:44:35 +0300

    Meeting 2 Task 1 ==Corrections

--- src/main/java/com/start/learn/human/Human.java
+++ src/main/java/com/start/learn/human/Human.java
@@ -3,58 +3,55 @@ package com.start.learn.human;
 import java.time.LocalDate;
 
 public class Human {
+
     private String name;
     private String surname;
     private final LocalDate dayOfBirth;
     private int humanId = assignId();
     private static int nextHumanId = 1;
 
+    public Human(int year, int month, int day) {
+        this.dayOfBirth = LocalDate.of(year, month, day);
+    }
+    public Human(String Name, String Surname, int yearOfBirth, int monthOfBirth, int dayOfBirth) {
+        this.name = Name;
+        this.surname = Surname;
+        this.dayOfBirth = LocalDate.of(yearOfBirth, monthOfBirth, dayOfBirth);
+    }
     private static int assignId() {
         int humanID = nextHumanId;
         nextHumanId++;
         return humanID;
     }
-
-    public static int getNextHumanId() {
-        return nextHumanId;
+    public  int getHumanId() {
+        return humanId;
     }
-
-    public Human(int year, int month, int day) {
-
-        dayOfBirth = LocalDate.of(year, month, day);
-
-    }
-
-    public Human(String eName, String eSurname, int yearOfBirth, int monthOfBirth, int dayOfBirth) {
-        name = eName;
-        surname = eSurname;
-        this.dayOfBirth = LocalDate.of(yearOfBirth, monthOfBirth, dayOfBirth);
-    }
-
     public String getName() {
         return name;
     }
-
     public String getSurname() {
         return surname;
     }
-
     public LocalDate getDayOfBirth() {
         return dayOfBirth;
     }
-
-    public void setNewNameAndSurname(Human person) {
-        this.name = "Mister";
-        this.surname = "White";
+    public void setNewName(String newFirstName) {
+        this.name = newFirstName;
     }
-
-    public static void printHumanInfo(Human person) {
+    public void setNewSurname(String newSurname) {
+        this.surname = newSurname;
+    }
+    public static String toString(Human person) {
         String generalPersonInfo;
         if (person.name != null && person.surname != null) {
             generalPersonInfo = String.format("Human name and surname= %s %s , BirthDay: %tD ", person.getName(), person.getSurname(), person.getDayOfBirth());
         } else {
-            generalPersonInfo = String.format("Human ID=%s, BirthDay: %tD ", getNextHumanId(), person.getDayOfBirth());
+            generalPersonInfo = String.format("Human ID=%s, BirthDay: %tD ", person.getHumanId(), person.getDayOfBirth());
         }
+        return generalPersonInfo;
+    }
+    public static void printPersonInfo(String  generalPersonInfo) {
         System.out.println(generalPersonInfo);
+        System.out.println();
     }
 }
--- src/test/java/HumanMainFunction.java
+++ src/test/java/HumanMainFunction.java
@@ -1,24 +1,34 @@
 import com.start.learn.human.Human;
 
-import static com.start.learn.human.Human.printHumanInfo;
+import static com.start.learn.human.Human.printPersonInfo;
 
 public class HumanMainFunction {
 
     public static void main(String[] args) {
         Human humanPersonOne = new Human("Petryk", "Pyatochky", 1990, 11, 25);
-        printHumanInfo(humanPersonOne);
-        humanPersonOne.setNewNameAndSurname(humanPersonOne);
-        printHumanInfo(humanPersonOne);
+        String personInfo= Human.toString(humanPersonOne);
+        System.out.println("Info about Person1 before name change");
+        printPersonInfo(personInfo);
+        humanPersonOne.setNewName("Vladislav");
+        personInfo=Human.toString(humanPersonOne);
+        System.out.println("Info about Person1 after name change");
+        printPersonInfo(personInfo);
 
         Human humanPersonTwo = new Human(1990, 11, 25);
-        printHumanInfo(humanPersonTwo);
+        personInfo=Human.toString(humanPersonTwo);
+        printPersonInfo(personInfo);
 
-        Human humanPersonThree = new Human("Coza", "Nostra", 1990, 11, 25);
-        printHumanInfo(humanPersonThree);
-        humanPersonThree.setNewNameAndSurname(humanPersonThree);
-        printHumanInfo(humanPersonThree);
+        Human humanPersonThree = new Human("Pepi", "Longstock", 1990, 11, 25);
+        personInfo= Human.toString(humanPersonThree);
+        System.out.println("Info about Person2 before surname change");
+        printPersonInfo(personInfo);
+        humanPersonThree.setNewSurname("Bigstock");
+        personInfo=Human.toString(humanPersonThree);
+        System.out.println("Info about Person2 after surname change");
+        printPersonInfo(personInfo);
 
-        Human humanPersonFour = new Human(1990, 11, 25);
-        printHumanInfo(humanPersonFour);
+        Human humanPersonFour = new Human(1992, 12, 28);
+        personInfo=Human.toString(humanPersonFour);
+        printPersonInfo(personInfo);
     }
 }
